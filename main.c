a3c2387ba11b72a47beaaaf46b8ff593be151a87
Ted Mader <ted.mader@ge.com>
2016-09-19 10:38:44 -0500

    Update redirect workflow and loop function

--- main.c
+++ main.c
@@ -53,33 +53,47 @@ char** split(char* line, char* delims) {
  * @param argv a vector of args
  * @return the vector of args for the command
  */
-char** redirect(char** argv) {
+int redirect(char** argv) {
   int i = 0;
   int in = 0;
   int out = 0;
-  int new_arg = 1;
+  int add_to_command = 1;
   int bufsize = 64;
-  char** new_argv = malloc(bufsize * sizeof(char**));
+  char** command = malloc(bufsize * sizeof(char**));
   while (argv[i] != NULL) {
-    if (strcmp(argv[i], "<") == 0) {
-      in = open(argv[i + 1], O_RDONLY);
-      dup2(in, 0);
-      close(in);
-      new_arg = 0;
-      new_argv[i] = NULL;
-    } else if (strcmp(argv[i], ">") == 0) {
-      out = open(argv[i + 1], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
-      dup2(out, 1);
-      close(out);
-      new_arg = 0;
-      new_argv[i] = NULL;
+    if (strcmp(argv[i], "<") == 0 || strcmp(argv[i], ">") == 0) {
+      if (add_to_command) {
+        add_to_command = 0;
+        command[i] = NULL;
+      }
+      int status;
+      pid_t pid = fork();
+      if (pid == -1) {
+        perror("mash");
+        exit(1);
+      } else if (pid == 0) {
+        if (strcmp(argv[i], "<") == 0) {
+          in = open(argv[i + 1], O_RDONLY);
+          dup2(in, 0);
+          close(in);
+        } else if (strcmp(argv[i], ">") == 0) {
+          out = open(argv[i + 1], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+          dup2(out, 1);
+          close(out);
+        }
+        execvp(*command, command);
+        printf("mash: command not found: %s\n", *command);
+        exit(1);
+      } else {
+        while (wait(&status) != pid);
+      }
     }
-    if (new_arg) {
-      new_argv[i] = argv[i];
+    if (add_to_command) {
+      command[i] = argv[i];
     }
     i++;
   }
-  return new_argv;
+  return 1;
 }
 
 /**
@@ -89,16 +103,13 @@ char** redirect(char** argv) {
 * @param use_redir a condition for using redirection
 * @return the status
 */
-int run(char** argv, int use_redir) {
+int run(char** argv) {
   int status;
   pid_t pid = fork();
   if (pid == -1) {
     perror("mash");
     exit(1);
   } else if (pid == 0) {
-    if (use_redir) {
-      argv = redirect(argv);
-    }
     execvp(*argv, argv);
     printf("mash: command not found: %s\n", *argv);
     exit(1);
@@ -187,7 +198,7 @@ int check_mash_commands(char** argv) {
     }
     i++;
   }
-  return run(argv, 0);
+  return 0;
 }
 
 /**
@@ -210,16 +221,20 @@ int loop() {
     }
     printf("-> %s ", dir);
     line = get_input();
-    if ((strstr(line, "|") && strstr(line, "<")) || (strstr(line, "|") && strstr(line, ">"))) {
-      printf("mash: cannot parse '|' with '<' or '>'\n");
-    } else if (strstr(line, "|")) {
-      status = pipe_commands(split(line, "|"));
-    } else if (strstr(line, "<") || strstr(line, ">")) {
-      status = run(split(line, " \t\r\n\a"), 1);
-    } else {
-      status = check_mash_commands(split(line, " \t\r\n\a"));
+    char** argv = split(line, " \t\r\n\a");
+    if (check_mash_commands(argv) == 0) {
+      if ((strstr(line, "|") && strstr(line, "<")) || (strstr(line, "|") && strstr(line, ">"))) {
+        printf("mash: cannot parse '|' with '<' or '>'\n");
+      } else if (strstr(line, "|")) {
+        status = pipe_commands(split(line, "|"));
+      } else if (strstr(line, "<") || strstr(line, ">")) {
+        status = redirect(argv);
+      } else {
+        status = run(argv);
+      }
     }
     free(line);
+    free(argv);
   }
   return status;
 }
